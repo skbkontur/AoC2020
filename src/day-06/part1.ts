fa9b2a1317e258279c32531c66475da745b8cbcb
Michael Uskov <uskovm@gmail.com>
2020-12-06 14:47:19 +0500

    Day 6 enhanced

--- src/day-06/part1.ts
+++ src/day-06/part1.ts
@@ -4,7 +4,7 @@ async function solvePart1() {
   const rawInput = (await readToString()).trim();
   const groups = rawInput.split("\n\n").map((s) => s.replace(/\n/g, ""));
   const result = groups.reduce(
-    (prev, current) => prev + new Set(current.replace("\n", "").split("")).size,
+    (prev, current) => prev + new Set(current).size,
     0
   );
   return result;
--- src/day-06/part2.ts
+++ src/day-06/part2.ts
@@ -1,27 +1,16 @@
 import { readToString } from "../stdin";
 
 async function solvePart2() {
-  const rawInput = (await readToString()).trim();
-  const groups = rawInput.split("\n\n");
-  const result = groups.reduce(
-    (prev, current) => prev + countForGroup(current),
-    0
-  );
-  return result;
-}
+    const input = (await readToString()).split('\n\n');
 
-function countForGroup(group: string): number {
-  const passengerAnswers = group.split("\n");
-  const resultSet = passengerAnswers
-    .slice(1)
-    .reduce(
-      (prev, current) => intersection(prev, new Set(current.split(""))),
-      new Set(passengerAnswers[0].split(""))
-    );
-    return resultSet.size;
+    let sum = 0;
+    for (const group of input) {
+        const answers = group.split('\n');
+        const firstLetters = answers[0].split('');
+        
+        sum += firstLetters.filter(l => answers.every(a => a.includes(l))).length;
+    }
+    return sum;
 }
-
-const intersection = (set1: Set<string>, set2: Set<string>) =>
-  new Set([...set1].filter((x) => set2.has(x)));
 
 solvePart2().then(console.log);
