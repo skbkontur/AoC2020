565e64017ccc1e0116b0d6a56914b959723a6849
Michael Uskov <uskovm@gmail.com>
2020-12-06 14:33:23 +0500

    Added Day 6 solutions

---
+++ src/day-06/part1.ts
@@ -0,0 +1,13 @@
+import { readToString } from "../stdin";
+
+async function solvePart1() {
+  const rawInput = (await readToString()).trim();
+  const groups = rawInput.split("\n\n").map((s) => s.replace(/\n/g, ""));
+  const result = groups.reduce(
+    (prev, current) => prev + new Set(current.replace("\n", "").split("")).size,
+    0
+  );
+  return result;
+}
+
+solvePart1().then(console.log);
---
+++ src/day-06/part2.ts
@@ -0,0 +1,27 @@
+import { readToString } from "../stdin";
+
+async function solvePart2() {
+  const rawInput = (await readToString()).trim();
+  const groups = rawInput.split("\n\n");
+  const result = groups.reduce(
+    (prev, current) => prev + countForGroup(current),
+    0
+  );
+  return result;
+}
+
+function countForGroup(group: string): number {
+  const passengerAnswers = group.split("\n");
+  const resultSet = passengerAnswers
+    .slice(1)
+    .reduce(
+      (prev, current) => intersection(prev, new Set(current.split(""))),
+      new Set(passengerAnswers[0].split(""))
+    );
+    return resultSet.size;
+}
+
+const intersection = (set1: Set<string>, set2: Set<string>) =>
+  new Set([...set1].filter((x) => set2.has(x)));
+
+solvePart2().then(console.log);
